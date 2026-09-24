71abb8830c525c768f567b995b4c39923e160b62
bavlysafwatt <bavlysafwatt@gmail.com>
2026-09-24 16:59:13 +0300

    first commit

---
+++ shared/src/commonMain/kotlin/com/bavly/chirp/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.bavly.chirp
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/commonMain/kotlin/com/bavly/chirp/Platform.kt
@@ -0,0 +1,7 @@
+package com.bavly.chirp
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
